bb93d4d339ea77c6aae7d5e6130b5169395c8703
Inderjeet Singh <inderjeet@singh>
2015-07-23 16:06:58 +0530

    update faults

--- gsa.cpp
+++ gsa.cpp
@@ -1,114 +1,170 @@
 #include <iostream>
 #include <stdlib.h>
-#include <math.h>
 #include <cmath>
 
 using namespace std;
 
 int main()
 {
-	int a[50], b[50];
-	double fit_value[50], best, wrost, mg[50], mgi[50], mgall, G[50], mj, Fij[50], temp1, Fi[50], aj[50], vdi[50], xt[50];
-	for(int i=0;i<50;i++)
+	int dm=2, in, it;
+	cout<<"\nEnter no of 'Particles' :- ";
+	cin>>in;
+	cout<<"\nEnter no of 'Iterations' :- ";
+	cin>>it;
+	double X[it][dm][in], fit[it][in], G[it], best[it], wrost[it], mg[it][in], mgall[it], Mg[it][in], Fij[it][dm][in][in-1], Fi[it][dm][in], A[it][dm][in], V[it][dm][in];
+	
+	for(int d=1;d<=2;d++)
 	{
-		int y, z;
-		y=rand()%11 - 5;
-		z=rand()%11 - 5;
-		a[i] = y;
-		b[i] = z;
-		cout<<i<<" no "<<i<<" a "<<a[i]<<" b "<<b[i]<<endl;
-	}
-	for(int i=0;i<50;i++
-	)
-	{
-		double x;
-		x=4*a[i]*a[i]-2.1*a[i]*a[i]*a[i]*a[i]+1/3*a[i]*a[i]*a[i]*a[i]*a[i]*a[i]+a[i]*b[i]-4*b[i]*b[i]+4*b[i]*b[i]*b[i]*b[i];
-		fit_value[i] = x;
-		cout<<"fit value ["<<i<<"] "<<fit_value[i]<<endl;
-	}
-	best = fit_value[0];
-	wrost = fit_value[0];
-	for (int i=1;i<50;i++)
-	{
-		if(best > fit_value[i])
+		for(int i=1;i<=in;i++)
 		{
-			best=fit_value[i];
+			double tmp = rand() / (double)RAND_MAX;
+			double x = tmp * (5 + 5);
+			x = x - 5;
+			X[1][d][i]=x;
+			cout<<"X[1]["<<d<<"]["<<i<<"]= "<<X[1][d][i]<<endl;
 		}
-		if(wrost< fit_value[i])
+	}
+	
+	for(int t=1;t<=it;t++)
+	{
+		
+		for(int i=1;i<=in;i++)
 		{
-			wrost = fit_value[i];
+			double tempfit=4*X[t][1][i]*X[t][1][i]-2.1*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]+1/3*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]+X[t][1][i]*X[t][2][i]-4*X[t][2][i]*X[t][2][i]+4*X[t][2][i]*X[t][2][i]*X[t][2][i]*X[t][2][i];
+			fit[t][i] = tempfit;
+			cout<<"fit value ["<<t<<"] ["<<i<<"]= "<<fit[t][i]<<endl;
 		}
+		
+		best[t] = fit[t][1];
+		wrost[t] = fit[t][1];
+		for (int i=1;i<=in;i++)
+		{
+			if(best[t] > fit[t][i])
+			{
+				best[t]=fit[t][i];  
+			}
+			if(wrost[t]< fit[t][i])
+			{
+				wrost[t] = fit[t][i];
+			}
+		}	
+		cout<<t<<"best= "<<best[t]<<"  wrost= "<<wrost[t]<<endl;
+		
+		double tempg, tempg2, tempg3;
+		tempg=t/50;
+		tempg2=tempg*-20;
+		tempg3= exp (tempg2);
+		G[t]=100*tempg3;
+		cout<<"G["<<t<<"]= "<<G[t]<<endl;
+		
+		for(int i=1;i<=in;i++)
+		{
+			double f_w, b_w;
+			f_w=fit[t][i]-wrost[t];
+			b_w=best[t]-wrost[t]*0.99;
+			mg[t][i]=f_w/b_w;
+			cout<<"mg["<<t<<"] ["<<i<<"]= "<<mg[t][i]<<endl;
+		}
+		
+		for(int i=1;i<=in;i++)
+		{
+			mgall[t] = mgall[t]+mg[t][i];
+		}
+		for(int i=1;i<=in;i++)
+		{
+			double mge = mgall[t]-mg[t][i];
+			Mg[t][i]=mg[t][i]/mge;
+			cout<<"Mg ["<<t<<"] ["<<i<<"]= "<<Mg[t][i]<<endl;
+		}
+		
+		for(int i=1;i<=in;i++)
+		{
+			for(int j=1;j<=in;j++)
+			{
+				if(j!=i)
+				{
+					double temp1, temp2, temp3,temp4;
+					temp1=X[t][2][j]-X[t][1][i];
+					temp2= abs(X[t][1][i]-X[t][2][j]);
+					temp2=temp2+0.2;
+					temp3=X[t][1][i]*X[t][2][j];
+					temp4=temp3/temp2;
+					float randFi = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+					Fij[t][1][i][j]=G[t]*temp1*temp4*randFi;
+					cout<<"Fij ["<<t<<"][1]["<<i<<"]["<<j<<"]= "<<Fij[t][1][i][j]<<endl;
+				}
+			}
+		}
+		
+		for(int j=1;j<=in;j++)
+		{
+			for(int i=1;i<=in;i++)
+			{
+				if(i!=j)
+				{
+					double temp1, temp2, temp3,temp4;
+					temp1=X[t][1][i]-X[t][2][j];
+					temp2= abs(X[t][2][j]-X[t][1][i]);
+					temp2=temp2+0.2;
+					temp3=X[t][2][j]*X[t][1][i];
+					temp4=temp3/temp2;
+					float randFi = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+					Fij[t][2][j][i]=G[t]*temp1*temp4*randFi;
+					cout<<"Fij ["<<t<<"][2]["<<j<<"]["<<i<<"]= "<<Fij[t][2][j][i]<<endl;
+				}
+			}
+		}
+		
+		for(int d=1;d<=2;d++)
+		{
+			for(int i=1;i<=in;i++)
+			{
+				double tempf=0;
+				for(int j=1;j<in;j++)
+				{
+					tempf=tempf+Fij[t][d][i][j];
+				}
+				Fi[t][d][i]=tempf;
+				cout<<"Fi ["<<t<<"]["<<d<<"]["<<i<<"]= "<<Fi[t][d][i]<<endl;
+			}
+		}
+
+		for(int d=1;d<=2;d++)
+		{
+			for(int i=1;i<=in;i++)
+			{
+				A[t][d][i]=(Fi[t][d][i])/(Mg[t][i]);
+			}
+		}
+		
+		
+		for(int d=1;d<=2;d++)
+		{
+			for(int i=1;i<=in;i++)
+			{
+				if(t==1)
+				{
+					V[t+1][d][i]=A[t][d][i];
+					cout<<"V["<<t+1<<"]["<<d<<"]["<<i<<"]= "<<V[t+1][d][i]<<endl;
+				}
+				if(t>1)
+				{
+					float randv = static_cast <float> (rand()) / static_cast <float> (RAND_MAX); 
+					V[t+1][d][i]=randv*V[t][d][i]+A[t][d][i];
+					cout<<"V["<<t+1<<"]["<<d<<"]["<<i<<"]= "<<V[t+1][d][i]<<endl;
+				}
+			}
+		}
+		
+		for(int d=1;d<=2;d++)
+		{
+			for(int i=1;i<=in;i++)
+			{
+				X[t+1][d][i]=X[t][d][i]+V[t+1][d][i];
+				cout<<"X["<<t+1<<"]["<<d<<"]["<<i<<"]= "<<X[t+1][d][i]<<endl;
+			}
+		}
+		
 	}	
-	cout<<"best="<<best<<"  wrost="<<wrost<<endl;
-	for(int i=0;i<50;i++)
-	{
-		double f_w, b_w;
-		f_w=fit_value[i]-wrost;
-		b_w=best-wrost;
-		mg[i]=f_w/b_w;
-		cout<<i<<"mg    "<<mg[i]<<endl;
-	}
-	for(int i=0;i<50;i++)
-	{
-		mgall = mgall+mg[i];
-	}
-	for(int i=0;i<50;i++)
-	{
-		double mge = mgall-mg[i];
-		mgi[i]=mg[i]/mge;
-		cout<<"mgi   "<<mgi[i]<<endl;
-	}
-	for(int i=0;i<50;i++)
-	{
-		double temp, temp2, temp3;
-		temp=i+1/50;
-		temp2=temp*-20;
-		temp3= exp (temp2);
-		G[i]=100*temp3;
-		cout<<"G["<<i<<"]= "<<G[i]<<endl;
-	}
-	for(int i=0;i<50;i++)
-	{
-		int temp=b[i]-a[i], temp2;
-		if(temp<0)
-		{
-			temp2=temp*-1;
-		}
-		temp2=temp2+0.2; 
-		mj=mg[i]*mg[i]/temp2;
-		Fij[i]=G[i]*mj*temp;
-		cout<<"Fij["<<i+1<<"]= "<<Fij[i]<<endl;
-	}
-	for(int i=0;i<50;i++)
-	{
-		float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-		float rf = roundf(r * 100) / 100;
-		temp1=temp1+rf;
-		cout<<rf<<endl;
-	}
-	for(int i=0;i<50;i++)
-	{
-		Fi[i]=temp1*Fij[i];
-		cout<<"Fi" <<Fi[i]<<endl;
-	}
-	for(int i=0;i<50;i++)
-	{
-		aj[i]=Fi[i]/mgi[i];
-		cout<<"aj"<<aj[i]<<endl;
-	}
-	vdi[0]=0;
-	for(int i=0;i<50;i++)
-	{
-		float randv = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-		float randvf = roundf(randv * 100) / 100;
-		vdi[i+1]=randvf*vdi[i]+aj[i];
-		cout<<"randm  "<<randvf<<"  vdi "<<vdi[i]<<endl;
-	}
-	xt[0]=0;
-	for(int i=0;i<50;i++)
-	{
-		xt[i+1]=xt[i]+vdi[i];
-		cout<<"xt "<<xt<<endl;
-	}
 	return 0;
 }
