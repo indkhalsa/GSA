32fc0289672fd4bacd2e810e36dcf8f1fb5b9b8d
Inderjeet Singh <inderjeet@singh>
2015-07-23 22:45:27 +0530

    reduce infinity

--- gsa.cpp
+++ gsa.cpp
@@ -11,14 +11,14 @@ int main()
 	cin>>in;
 	cout<<"\nEnter no of 'Iterations' :- ";
 	cin>>it;
-	double X[it][dm][in], fit[it][in], G[it], best[it], wrost[it], mg[it][in], mgall[it], Mg[it][in], Fij[it][dm][in][in-1], Fi[it][dm][in], A[it][dm][in], V[it][dm][in];
+	double long X[it][dm][in], fit[it][in], G[it], best[it], wrost[it], mg[it][in], mgall[it], Mg[it][in], Fij[it][dm][in][in-1], Fi[it][dm][in], A[it][dm][in], V[it][dm][in];
 	
 	for(int d=1;d<=2;d++)
 	{
 		for(int i=1;i<=in;i++)
 		{
-			double tmp = rand() / (double)RAND_MAX;
-			double x = tmp * (5 + 5);
+			double long tmp = rand() / (double)RAND_MAX;
+			double long x = tmp * (5 + 5);
 			x = x - 5;
 			X[1][d][i]=x;
 			cout<<"X[1]["<<d<<"]["<<i<<"]= "<<X[1][d][i]<<endl;
@@ -30,9 +30,9 @@ int main()
 		
 		for(int i=1;i<=in;i++)
 		{
-			double tempfit=4*X[t][1][i]*X[t][1][i]-2.1*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]+1/3*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]+X[t][1][i]*X[t][2][i]-4*X[t][2][i]*X[t][2][i]+4*X[t][2][i]*X[t][2][i]*X[t][2][i]*X[t][2][i];
+			double long tempfit=4*X[t][1][i]*X[t][1][i]-2.1*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]+1/3*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]*X[t][1][i]+X[t][1][i]*X[t][2][i]-4*X[t][2][i]*X[t][2][i]+4*X[t][2][i]*X[t][2][i]*X[t][2][i]*X[t][2][i];
 			fit[t][i] = tempfit;
-			cout<<"fit value ["<<t<<"] ["<<i<<"]= "<<fit[t][i]<<endl;
+			cout<<"fit value ["<<t<<"]["<<i<<"]= "<<fit[t][i]<<endl;
 		}
 		
 		best[t] = fit[t][1];
@@ -50,31 +50,42 @@ int main()
 		}	
 		cout<<t<<"best= "<<best[t]<<"  wrost= "<<wrost[t]<<endl;
 		
-		double tempg, tempg2, tempg3;
+		double long tempg, tempg2, tempg3;
 		tempg=t/50;
 		tempg2=tempg*-20;
 		tempg3= exp (tempg2);
 		G[t]=100*tempg3;
 		cout<<"G["<<t<<"]= "<<G[t]<<endl;
 		
+		double b_w=best[t]-wrost[t];
 		for(int i=1;i<=in;i++)
 		{
-			double f_w, b_w;
+			double long f_w;
 			f_w=fit[t][i]-wrost[t];
-			b_w=best[t]-wrost[t]*0.99;
 			mg[t][i]=f_w/b_w;
-			cout<<"mg["<<t<<"] ["<<i<<"]= "<<mg[t][i]<<endl;
+			cout<<"fit-worst"<<f_w<<"  best-worst"<<b_w<<endl;
+			cout<<"mg["<<t<<"]["<<i<<"]= "<<mg[t][i]<<endl;
 		}
 		
+		mgall[t]=0;
 		for(int i=1;i<=in;i++)
 		{
 			mgall[t] = mgall[t]+mg[t][i];
+			cout<<"mgall"<<mgall[t]<<endl;
 		}
 		for(int i=1;i<=in;i++)
 		{
-			double mge = mgall[t]-mg[t][i];
-			Mg[t][i]=mg[t][i]/mge;
-			cout<<"Mg ["<<t<<"] ["<<i<<"]= "<<Mg[t][i]<<endl;
+			double long mge = mgall[t]-mg[t][i];
+			if(mge != 0)
+			{
+				Mg[t][i]=mg[t][i]/mge;
+				cout<<"mge"<<mge<<endl;
+				cout<<"Mg ["<<t<<"]["<<i<<"]= "<<Mg[t][i]<<endl;
+			}
+			else
+			{
+				Mg[t][i]=mg[t][i];
+			}
 		}
 		
 		for(int i=1;i<=in;i++)
@@ -83,7 +94,7 @@ int main()
 			{
 				if(j!=i)
 				{
-					double temp1, temp2, temp3,temp4;
+					double long temp1, temp2, temp3,temp4;
 					temp1=X[t][2][j]-X[t][1][i];
 					temp2= abs(X[t][1][i]-X[t][2][j]);
 					temp2=temp2+0.2;
@@ -102,7 +113,7 @@ int main()
 			{
 				if(i!=j)
 				{
-					double temp1, temp2, temp3,temp4;
+					double long temp1, temp2, temp3,temp4;
 					temp1=X[t][1][i]-X[t][2][j];
 					temp2= abs(X[t][2][j]-X[t][1][i]);
 					temp2=temp2+0.2;
@@ -119,7 +130,7 @@ int main()
 		{
 			for(int i=1;i<=in;i++)
 			{
-				double tempf=0;
+				double long tempf=0;
 				for(int j=1;j<in;j++)
 				{
 					tempf=tempf+Fij[t][d][i][j];
